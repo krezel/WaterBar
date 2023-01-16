171a3f81a2ff8d755c04fde0c3ca2e2e8fb871e1
dominikkrezel <krezel.dominik1@gmail.com>
2023-01-16 23:11:34 +0100

    Water progress bar

--- app/src/main/java/com/example/waterbar/MainActivity.kt
+++ app/src/main/java/com/example/waterbar/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.waterbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val progressBar: ProgressBar = findViewById(R.id.water_bar)
+        val waterInput: EditText = findViewById(R.id.water_ml)
+        val btnSave: Button = findViewById(R.id.zapisz_wode)
+        btnSave.setOnClickListener {
+            progressBar.incrementProgressBy(waterInput.text.toString().toInt())
+        }
     }
 }
