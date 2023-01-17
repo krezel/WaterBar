ac213aa52138ea4375fb1724be6b1ed61a19b376
dominikkrezel <krezel.dominik1@gmail.com>
2023-01-17 17:11:07 +0100

    water counter

--- app/src/main/java/com/example/waterbar/MainActivity.kt
+++ app/src/main/java/com/example/waterbar/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,15 +14,23 @@ class MainActivity : AppCompatActivity() {
         val progressBar: ProgressBar? = findViewById(R.id.water_bar)
         val waterInput: EditText = findViewById(R.id.water_ml)
         val btnSave: Button = findViewById(R.id.zapisz_wode)
+        val waterCounter: TextView = findViewById(R.id.water_counter)
+        var waterQuan = 0
         btnSave.setOnClickListener {
             try {
-                if ((1..2000).contains(waterInput.text.toString().toInt()))
+                if ((1..2000).contains(waterInput.text.toString().toInt())){
                     progressBar?.incrementProgressBy(waterInput.text.toString().toInt())
+                waterQuan += waterInput.text.toString().toInt()
+                    if(waterQuan>1999)
+                        waterQuan=0
+                }
                 else
                     progressBar?.incrementProgressBy(0)
             }catch (e: java.lang.NumberFormatException){
                 progressBar?.incrementProgressBy(0)
             }
+            val string = String.format("$waterQuan/2000")
+            waterCounter.text = string
             if(progressBar?.progress==progressBar?.max)
                 progressBar?.progress = 0
         }
