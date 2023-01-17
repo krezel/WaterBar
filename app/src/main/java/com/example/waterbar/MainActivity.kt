0e72432b70657d631879f001f4650cce8027e8db
dominikkrezel <krezel.dominik1@gmail.com>
2023-01-17 16:47:22 +0100

    try catch input

--- app/src/main/java/com/example/waterbar/MainActivity.kt
+++ app/src/main/java/com/example/waterbar/MainActivity.kt
@@ -10,11 +10,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val progressBar: ProgressBar = findViewById(R.id.water_bar)
+        val progressBar: ProgressBar? = findViewById(R.id.water_bar)
         val waterInput: EditText = findViewById(R.id.water_ml)
         val btnSave: Button = findViewById(R.id.zapisz_wode)
         btnSave.setOnClickListener {
-            progressBar.incrementProgressBy(waterInput.text.toString().toInt())
+            try {
+                if ((1..2000).contains(waterInput.text.toString().toInt()))
+                    progressBar?.incrementProgressBy(waterInput.text.toString().toInt())
+                else
+                    progressBar?.incrementProgressBy(0)
+            }catch (e: java.lang.NumberFormatException){
+                progressBar?.incrementProgressBy(0)
+            }
+            if(progressBar?.progress==progressBar?.max)
+                progressBar?.progress = 0
         }
     }
 }
